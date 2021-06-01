3faf22a7c4811013f1a3556b339e3c91099c9776
CaptainsDT <760245899@qq.com>
2021-06-01 13:29:23 +0800

    main

--- test.py
+++ test.py
@@ -1,13 +1,2 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-# 5位数依次打印出个数字
-
-import os
-import sys
-
-num = 12345
-w = 10000
-for i in range(5):
-    print(num // w)
-    num = num % w
-    w = w // 10
