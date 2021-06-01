068bb14b5a57d73439b1154d8e616e731760bffb
CaptainsDT <760245899@qq.com>
2021-06-01 13:19:18 +0800

    main

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 # 
+# 5位数依次打印出个数字
 
 import os
 import sys
 
-num = input("")
-
-print(num)
+num = 12345
+w = 10000
+for i in range(5):
+    print(num // w)
+    num = num % w
+    w = w // 10
