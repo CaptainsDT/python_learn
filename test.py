f9d4881a1f964236246627e33a8788e93273869c
CaptainsDT <760245899@qq.com>
2021-06-18 15:16:11 +0800

    main

--- test.py
+++ test.py
@@ -2,7 +2,28 @@
 # -*- coding: UTF-8 -*-
 
 import time
-A = 5
-print(1)
-time.sleep(5)
+import keyword
+import sys
+
+'''
+# \n换行，r禁止换行
+print("换行：""ABVCADA\nFAFASFA")
+print("禁止换行："r"ABVCADA\nFAFASFA")
+'''
+
+'''
+# 取值
+str = '1234567890'
+
+print(str)
+print(str[1])
+print(str +)
+'''
+
+'''
+A = input()
 print(A)
+'''
+
+x = 'runoob'
+sys.stdout.write(x + '\n')
